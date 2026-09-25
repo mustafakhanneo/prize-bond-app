8f7265dfa3760156fcf20def4543a321c75f06ab
mustafakhanneo@gmail.com <mustafakhanneo@gmail.com>
2026-09-25 18:54:19 +0500

    SFd

--- OneDrive/Desktop/prize-bond-app/app/robots.ts
+++ OneDrive/Desktop/prize-bond-app/app/robots.ts
@@ -1,12 +1,13 @@
 import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
+    const WEBSITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://YOUR-DOMAIN.com";
   return {
     rules: {
       userAgent: '*',
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: `${WEBSITE_URL}/sitemap.xml`,
   }
 }
